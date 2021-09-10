47998310fc2dfd036f33e41fb593039dce91b553
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-09 21:52:12 -0300

    Primeira commit da aula de Laravel

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+//Acionando a view Welcome digitar /home
+//Route::get('/home', function () {
+  //  return view('welcome');
+//});
+//Acionando a view Welcome digitar /home/sistemas Exemplo
+//Route:: get('/home/sistema', function(){
+ //   return view("welcome");
+//});
+
+//Acionando a view Jogos digitar /jogos
+Route::get('/jogos', function(){
+        return view("jogos");
+});
